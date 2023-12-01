d711a591b40777262e4011f48b175fe69d3c5921
RonaldoMacedo <ronni29_100@hotmail.com>
2023-12-01 10:43:37 -0300

    Aula 257: function - implementação da interface

--- src/application/Program05.java
+++ src/application/Program05.java
@@ -3,14 +3,15 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program05 {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 
@@ -18,12 +19,10 @@ public class Program05 {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		
-		double factor = 1.1;
-		
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
-		
-		list.forEach(System.out::println);
+		names.forEach(System.out::println);
 
 	}
 
